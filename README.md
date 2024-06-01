f28efdf8c4e7f522de5ec819d6368df89c6b30db
Daniel Kukula <daniel.kuku@gmail.com>
2024-06-01 12:09:06 +0100

    add docs

--- README.md
+++ README.md
@@ -9,6 +9,14 @@ Backport module currently includes dbg implementation for:
 Wraps your code in IO.inspect calls used for showing the intermediate results.
 The difference between the upstream dbg function is that it displays as the code
 is executed and is not waiting until a block of code finishes.
+It also works with whole modules and functions
+
+```elixir
+defmodule X do
+    def y do: :z
+end
+|> dbg
+```
 
 ### DbgMate.Inspect.dbg_tc
 Wraps your code in IO.inspect calls used for showing the intermediate results
@@ -16,7 +24,7 @@ and additionally shows the time it took to execute every line.
 
 To use it just set the config value in config or run this before your dbg call
 
-```
+```elixir
 Application.put_env(:elixir, :dbg_callback, {DbgMate,Backport, :dbg, []})
 ```
 
--- lib/dbg_mate/inspect.ex
+++ lib/dbg_mate/inspect.ex
@@ -1,4 +1,31 @@
 defmodule DbgMate.Inspect do
+  @moduledoc """
+  dbg functtions that use IO.inspect() do display results
+
+
+
+
+  @doc \"""
+  Wraps your code in IO.inspect calls used for showing the intermediate results.
+  The difference between the upstream dbg function is that it displays as the code
+  is executed and is not waiting until a block of code finishes.
+  It also works with whole modules and functions
+
+  ```elixir
+  defmodule X do
+    def y do: :z
+  end
+  |> dbg
+
+  defmodule Y do
+  def x do
+    :z
+  end
+  |> dbg
+  end
+  ```
+  """
+
   @valid_ops [:def, :defp, :if, :unless, :case, :cond, :with, :=, :for, :|>]
 
   def dbg([do: {op, meta, clauses}], options, env) do
@@ -25,6 +52,13 @@ defmodule DbgMate.Inspect do
     ast
   end
 
+  @doc """
+  Similar to previous one that it wraps your code in IO.inspect calls used for sho
+  wing the intermediate results
+  and additionally shows the time it took to execute every line.
+
+  To use it just set the config value in config or run this before your dbg call
+  """
   def dbg_tc([do: {op, meta, clauses}], options, env) do
     [do: dbg_tc({op, meta, clauses}, options, env)]
   end
